ae19db176b65d6b64fd7550312363ace4081f281
Priyesh Wagh <priyeshwagh777@hotmail.com>
2021-12-31 16:27:03 +0530

    C# Helper Plugin file with initializing methods
    You can copy the helper methods in this file and copy-paste in your own C# Dynamics 365 CRM Plugins Assembly Project.

---
+++ AccountUpdate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace CFT158SalesPlugins
+{
+    public class AccountUpdate : IPlugin
+    {
+        IOrganizationService service; Entity currentEntity; 
+        IPluginExecutionContext context; 
+        ITracingService trace; 
+
+        /// <summary>
+        /// Initializes common parameters to set the IPluginExecutionContext and IOrganizationServiceFactory.
+        /// </summary>
+        /// <param name='serviceProvider'>Object of IServiceProvider</param>
+        private void GetOrganizationService(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+                service = ((IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(context.UserId);
+                trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                GetOrganizationService(serviceProvider);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
